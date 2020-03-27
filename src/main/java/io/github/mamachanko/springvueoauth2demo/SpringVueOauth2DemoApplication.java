91fb838cf66431c7e2d9c3d390396901dd9e7c52
mamachanko <sugardubz@gmail.com>
2020-03-27 08:04:22 +0100

    issue jwt access token and protect endpoints

---
+++ src/main/java/io/github/mamachanko/springvueoauth2demo/SpringVueOauth2DemoApplication.java
@@ -0,0 +1,164 @@
+package io.github.mamachanko.springvueoauth2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@SpringBootApplication
+public class SpringVueOauth2DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringVueOauth2DemoApplication.class, args);
+    }
+
+}
+
+@EnableResourceServer
+@Configuration
+class ResourceServerConfig {
+}
+
+@Configuration
+class PasswordConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
+
+@Configuration
+@EnableAuthorizationServer
+class AuthServer extends AuthorizationServerConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthServer(PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager) {
+        this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        endpoints
+                .authenticationManager(authenticationManager)
+                .tokenStore(tokenStore())
+                .accessTokenConverter(accessTokenConverter());
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients
+                .inMemory()
+                .withClient("client")
+                .secret(passwordEncoder.encode("secret"))
+                .authorizedGrantTypes("password")
+                .scopes("user_info");
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey("secret-signing-key");
+        return converter;
+    }
+
+    @Bean
+    @Primary
+    public DefaultTokenServices tokenServices() {
+        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenStore(tokenStore());
+        defaultTokenServices.setSupportRefreshToken(true);
+        return defaultTokenServices;
+    }
+
+}
+
+@Configuration
+@EnableWebSecurity
+@Order(1)
+class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    public WebSecurity(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user")
+                .password(passwordEncoder.encode("password"))
+                .roles("USER");
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .requestMatchers()
+                .antMatchers("/user-info", "/login", "/oauth/authorize", "/api/public")
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/public").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().permitAll();
+    }
+}
+
+@RestController
+class ApiController {
+
+    @GetMapping("/api/me")
+    public Principal getUserInfo(Principal principal) {
+        return principal;
+    }
+
+    @GetMapping("/api/private")
+    public String getPrivate() {
+        return "this is private";
+    }
+
+    @GetMapping("/api/public")
+    public String getPublic() {
+        return "this is for everyone";
+    }
+
+}
---
+++ src/test/java/io/github/mamachanko/springvueoauth2demo/SpringVueOauth2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mamachanko.springvueoauth2demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVueOauth2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
