19a8410b5f757deecfa0020b83f6a0ddb151ebc8
mamachanko <sugardubz@gmail.com>
2020-03-29 15:50:48 +0200

    style login form

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,25 +1,27 @@
 <template>
   <div class="login">
     <h1>Let's log you in, Anon!</h1>
-    <div class="formControl">
-      <label for="username">Username: </label>
-      <input
-        type="text"
-        id="username"
-        v-model="username"
-        placeholder="username"
-      />
+    <div class="form">
+      <div class="form-control">
+        <label for="username">Username: </label>
+        <input
+          type="text"
+          id="username"
+          v-model="username"
+          placeholder="username"
+        />
+      </div>
+      <div class="form-control">
+        <label for="password">Password:</label>
+        <input
+          type="text"
+          id="password"
+          v-model="password"
+          placeholder="password"
+        />
+      </div>
+      <button @click="login">Login</button>
     </div>
-    <div class="formControl">
-      <label for="password">Password:</label>
-      <input
-        type="text"
-        id="password"
-        v-model="password"
-        placeholder="password"
-      />
-    </div>
-    <button @click="login">Login</button>
   </div>
 </template>
 
@@ -42,3 +44,45 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.form-control {
+  margin-bottom: 8px;
+  padding-bottom: 20px;
+  position: relative;
+  display: flex;
+  align-items: baseline;
+}
+
+.form-control label {
+  display: block;
+  margin-bottom: 5px;
+  width: 100px;
+}
+
+.form-control input {
+  border: 2px solid #f0f0f0;
+  border-radius: 4px;
+  display: block;
+  width: 100%;
+  padding: 10px;
+}
+
+.form-control input:focus {
+  outline: 0;
+  border-color: #777777;
+}
+
+.form button {
+  cursor: pointer;
+  background-color: #3498db;
+  border: 2px solid #3498db;
+  border-radius: 4px;
+  color: #ffffff;
+  display: block;
+  font-size: 16px;
+  padding: 10px;
+  margin-top: 20px;
+  width: 100%;
+}
+</style>
